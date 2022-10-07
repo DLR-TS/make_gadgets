ddce7355c6898181fe2fec5b6775bc210b29f1b6
Andrew Koerner <andrew.koerner@dlr.de>
2022-10-07 13:28:02 +0200

    Version 1

--- docker/README.md
+++ docker/README.md
@@ -6,7 +6,7 @@ imported to extend your makefile or called directly.
 ## Docker tools Makefile
 This directory contains a Makefile that enhance vanilla docker with make.
 
-The Makefile provided in this directory contains a wrapper for the docker_image_cacher.sh
+The Makefile provided in this directory contains a wrapper for the docker-image-cacher.sh
 also included in this project making it easier to use this tool.
 
 To consume this Makefile you can directly import it to your Makefile such as follows: 
@@ -24,21 +24,30 @@ make docker_load
 make docker_orbital_cannon
 ```
 
-## docker_image_cacher.sh
-The docker_image_cacher.sh is a utility script for caching docker images with 
+The Makefile also provides several practical targets that wrap the functionality of the docker-image-cacher.sh tool
+provided in this project.
+
+## docker-image-cacher.sh
+The docker-image-cacher.sh is a utility script for caching docker images with 
 the goal of saving network resources.
 
-There are three major functions this script offers namely fetching, saving, and loading.  All of these
-will be discussed in the coming sections
+There are three major functions this script offers namely fetching, saving, loading, and printing.  All of these
+will be discussed in the coming sections.
+
+### Printing
+you can call docker-image-cacher.sh with the -p or --print flag to perform a print operation.
+
+The print operation or action
+
 
 
 ### Fetching
-You can call docker_image_cacher.sh with the -f or --fetch flag to perform a fetch operation.
+You can call docker-image-cacher.sh with the -f or --fetch flag to perform a fetch operation.
 
 There are two ways this can be done. The fist way provides a docker image search path as a basis
 to collect a list of images to fetch as in the following example:
 ```bash
-bash docker_image_cacher.sh --docker-image-search-path "${HOME}" --fetch
+bash docker-image-cacher.sh --docker-image-search-path "${HOME}" --fetch
 ```
 All Dockerfiles in the search path provided will be recursively searched for the docker key word "FROM". Any image 
 that is referenced by "FROM IMAGE:TAG" in any Dockerfile within the search path will be fetched with docker using 
@@ -46,33 +55,43 @@ the docker pull command.
 
 The second way to fetch docker images is to provide no search path such as follows:
 ```bash
-bash docker_image_cacher.sh --fetch
+bash docker-image-cacher.sh --fetch
 ```
 This will effectively update every image already loaded into docker on your host.
 
 ### Saving
 Similar to fetching saving can be called in two ways.  The first way is to provide a search path
 ```bash
-bash docker_image_cacher.sh --docker-image-search-path "${HOME}" --save
+bash docker-image-cacher.sh --docker-image-search-path "${HOME}" --save
 ```
 resulting in every discovered docker image will be saved into the default docker image cache directory as an archive 
 for each image. 
 
 Another way to call save is without a search path such as follows:
-```bash
-bash docker_image_cacher.sh --save
+```bash bash docker-image-cacher.sh --save
 ```
 Every image in the docker registry will be saved to disk as a tar archive into
 the default docker image cache directory.
 
+In order for save to work all docker images in the image list must exist in the docker registry. An image can be added
+to the registry in multiple ways:
+- Calling 'docker build' on a Dockerfile containing an image reference will pull it to the local registry
+- Calling 'docker pull <repository:tag>'
+- Using docker-image-cacher.sh with the --fetch flag to batch pull a list of images.
+
 ### Loading
 All images that are cached as tar archives in the default docker image cache directory will be loaded into the docker registry.
 ```bash
-bash docker_image_cacher.sh --load
+bash docker-image-cacher.sh --load
 ```
 
 ### Help
 To view more documentation on this tool you can run the help flag:
 ```bash
-bash docker_image_cacher.sh --help
+bash docker-image-cacher.sh --help
 ```
+
+## Use Cases
+This section will give a few practical use cases for the docker-image-cacher.sh tool
+
+
--- docker/docker-image-cacher.sh
+++ docker/docker-image-cacher.sh
@@ -26,47 +26,45 @@ FETCH=false
 PRINT=false
 
 help (){
-    
+ 
     printf "  \n"
-    printf "docker_image_cacher.sh is a Docker image caching tool. \n\n"
-    printf "  Usage: bash docker_image_cacher.sh [OPTIONS]\n\n"
+    printf "docker-image-cacher.sh is a Docker image caching tool. \n\n"
+    printf "  \n"
+    printf "  Usage: bash docker-image-cacher.sh [OPTIONS]\n\n"
 
     printf "         Scrape your home directory for docker images, fetch them, and save them:\n"
-    printf "             bash docker_image_cacher.sh --fetch --save -docker-image-search-path \$HOME\n\n"
-    printf "         Load docker images from cache into the local registry: \n"
-    printf "             bash docker_image_cacher.sh --load \n\n"
+    printf "             bash docker-image-cacher.sh --fetch --save -docker-image-search-path \$HOME\n\n"
+
+    printf "         Save all docker images in the local registry to the default cache location:\n"
+    printf "             bash docker-image-cacher.sh --save"
+
+    printf "         Load docker images from the default cache location into the local registry: \n"
+    printf "             bash docker-image-cacher.sh --load \n\n"
 
     printf "  Description: This tool can be used to discover, fetch, cache, and load docker images in your local \n"
-    printf "               registry with the goal of saving bandwidth. \n"
-    printf "  \n"
-    printf "  Options: \n"
-    printf "           -p, --print-discoverable-docker-images \n"
-    printf "                                                  This flag will print all discoverable docker images and \n"
-    printf "                                                  exit 0. The strategy used to discovered images is \n"
-    printf "                                                  determined by the '-d' flag. If a docker image search \n"
-    printf "                                                  path is provided with -d then that path is scraped for \n"
-    printf "                                                  potential docker images. If no '-d' flag is provided\n"
-    printf "                                                  then the local docker registry is scraped. Discovered \n"
-    printf "                                                  images are used for fetching and caching. \n\n"
+    printf "               registry with the goal of saving bandwidth. Docker images are saved and loaded from tar archives \n\n"
 
-    printf "           -i, --docker-image-inclusion-list [inclusion list] \n"
+    printf "  Options: \n"
+
+    printf "           -i, --docker-image-inclusion-list [inclusion list] OPTIONAL\n"
     printf "                                                  The docker image exclusion list is a space separated list \n"
-    printf "                                                  of docker images to include in fetching and caching. \n"
+    printf "                                                  of docker images to include in fetching and caching in \n"
+    printf "                                                  addition to images scraped the docker image search path \n"
     printf "                                                  example: 'ubuntu:latest debian:latest' \n\n"
 
-    printf "           -x, --docker-image-exclusion-list [exclusion list] \n"
-    printf "                                                  The docker image inclusion list is a space separated list \n"
+    printf "           -x, --docker-image-exclusion-list [exclusion list] OPTIONAL\n"
+    printf "                                                  The docker image exclusion list is a space separated list \n"
     printf "                                                  of docker images to be excluded from fetching and caching. \n"
     printf "                                                  example: 'ubuntu:latest debian:latest' \n\n"
 
-    printf "           -d, --docker-image-search-path [directory] \n"
+    printf "           -d, --docker-image-search-path [directory] OPTIONAL\n"
     printf "                                                  This flag provides a search path to discover docker \n"
     printf "                                                  images.  This path will be recursively scraped for \n"
     printf "                                                  Dockerfiles containing 'from <repository>:<tag>'. If no \n"
     printf "                                                  docker image search path is provide then docker images \n"
-    printf "                                                  are pulled from the local registry. \n\n"
+    printf "                                                  are pulled from the local registry via 'docker image ls'. \n\n"
 
-    printf "           -c, --docker-image-cache-directory [directory]"
+    printf "           -c, --docker-image-cache-directory [directory] OPTIONAL\n"
     printf "                                                  The docker image cache directory is were image archives \n" 
     printf "                                                  are saved to and loaded from by this tool. \n"
     printf "                                                  If no docker image cache directory is provided then one \n"
@@ -74,6 +72,8 @@ help (){
     printf "                                                  '%s'. \n" "$(basename ${DOCKER_IMAGE_CACHE_DIRECTORY})"
     printf "                                                  If a cache directory is provided with -c then it will be \n"
     printf "                                                  created including all parent directives using mkdir -p \n\n"
+
+    printf "    Operations: REQUIRED - At least one operation is required! \n"
 
     printf "           -f, --fetch                            \n"
     printf "                                                  If this flag is provided the list of discovered docker \n"
@@ -87,6 +87,15 @@ help (){
     printf "           -l, --load                             Load all images in the docker image cache directory from \n"
     printf "                                                  their respective tar archives into the local docker \n"
     printf "                                                  registry \n\n"
+
+    printf "           -p, --print \n"
+    printf "                                                  This flag will print all discoverable docker images and \n"
+    printf "                                                  exit 0. The strategy used to discovered images is \n"
+    printf "                                                  determined by the '-d' flag. If a docker image search \n"
+    printf "                                                  path is provided with -d then that path is scraped for \n"
+    printf "                                                  potential docker images. If no '-d' flag is provided\n"
+    printf "                                                  then the local docker registry is scraped. Discovered \n"
+    printf "                                                  images are used for fetching and caching. \n\n"
 
     printf "           -c, --conditional-load                 Same as load however if there is already any docker \n"
     printf "                                                  images in the local registry then this is a NOOP. \n\n"
@@ -123,7 +132,7 @@ while [[ $# -gt 0 ]]; do
       LOAD=true
       shift # past argument
       ;;
-    -p|--print-discoverable-docker-images)
+    -p|--print)
       PRINT=true
       shift # past argument
       ;;
@@ -171,13 +180,12 @@ find_docker_base_images(){
     local docker_image_inclusion_list="${3}"
     local docker_images=""
 
-    docker_image_exclusion_list="$(echo "${docker_image_exclusion_list}" | tr ' ' '\n')"
-    docker_image_inclusion_list="$(echo "${docker_image_inclusion_list}" | tr ' ' '\n')"
+    docker_image_exclusion_list="$(echo "${docker_image_exclusion_list}" | tr ' ' '\n' | sort)"
+    docker_image_inclusion_list="$(echo "${docker_image_inclusion_list}" | tr ' ' '\n' | sort)"
 
     echodebug "  FROM find_docker_base_images:" 
 
     if [[ ! -z "${search_path}" ]]; then
-        echo "${search_path}"
         search_path="$(realpath "${search_path}" 2> /dev/null || echo "${search_path}")" 
     fi
    
@@ -198,7 +206,7 @@ find_docker_base_images(){
         done
     fi
     
-    docker_images="$(echo -e "${docker_images}\n${docker_image_inclusion_list}" | sed '/^$/d' | grep ":" | grep -v "<none>:<none>" | sort | uniq)"
+    docker_images="$(echo -e "${docker_images}\n${docker_image_inclusion_list}" | sed '/^$/d' | grep ":" | grep -v -e '<none>:<none>\|\$' | sort | uniq)"
     #docker_images="$(printf "%s" "${docker_images}" | sed '/^$/d' | sort | uniq)"
     echodebug "    docker_images: ${docker_images}"
     echodebug "    docker_images count: $(echo "${docker_images}" | wc -L)"
@@ -214,7 +222,14 @@ fetch_docker_images(){
     
     for docker_image in $docker_images; do
         echo "    fetching docker image: ${docker_image}"
-        docker pull "${docker_image}" 2>/dev/null || true
+        { # try
+            docker pull "${docker_image}" 2>/dev/null
+        } || { # catch
+            local error_message="    ERROR: Unable to pull docker image: ${docker_image} from Docker central registry, try "
+            error_message+="adding it to the exclusion list. \n" 
+            error_message+="      Example: 'bash docker-image-cacher.sh ... -x \"${docker_image}\" ...' \n\n"
+            echoerr "${error_message}"
+        }
     done
 }
 
@@ -237,11 +252,17 @@ save_docker_images(){
     for docker_image in $docker_images; do
         docker_image_archive="${docker_image_cache_directory}/$(echo "${docker_image//:/_}" | sed "s|/|_|g").tar"
         echo "    saving docker image: ${docker_image} to ${docker_image_archive}"
-        docker save --output "${docker_image_archive}" "${docker_image}" 2>/dev/null || true
+        { # try
+            docker save --output "${docker_image_archive}" "${docker_image}"
+        } || { # catch
+            local error_message="    ERROR: Unable to save docker image: ${docker_image} "
+            error_message+="it must exist in the local registry to save it.\n" 
+            error_message+="      Try 'docker pull ${docker_image}' and run this tool again. "
+            error_message+="You can also run this tool with the 'fetch' operation to pull this docker image. \n\n"
+            echoerr "${error_message}"
+        }
+        #docker save --output "${docker_image_archive}" "${docker_image}" 2>/dev/null || true
     done
-
-    docker pull edrevo/dockerfile-plus && docker save --output edrevo_dockerfile-plus.tar edrevo/dockerfile-plus
-
 }
 
 load_docker_images(){
@@ -297,5 +318,6 @@ fi
 
 if [ "${FETCH}" == false -a "${SAVE}" == false -a "${LOAD}" == false -a "${CONDITIONAL_LOAD}" == false -a "${PRINT}" == false ]; then
     help
+    echoerr "ERROR: Invalid or no arguments. You must provide an operation: save (-s, --save), load (-l, --load), fetch (-f, --fetch), or print (-p, --print). \n"
 fi
 
