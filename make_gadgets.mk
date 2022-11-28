d955ba610d074b61b94e1d27dada3538e62e877d
alhadrad@gmail.com <akoerner>
2022-11-28 15:08:20 +0100

    added uniq filter to remove duplicate targets

--- make_gadgets.mk
+++ make_gadgets.mk
@@ -11,7 +11,7 @@ REPO_DIRECTORY?="${MAKE_GADGETS_MAKEFILE_PATH}"
 
 .PHONY: help  
 help:
-	@awk 'BEGIN {FS = ":.*##"; printf "Usage: make \033[36m<target>\033[0m\n"} /^[a-zA-Z0-9_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+	@printf "Usage: make \033[36m<target>\033[0m\n%s\n" "$$(awk 'BEGIN {FS = ":.*##"} /^[a-zA-Z0-9_-]+:.*?##/ { printf "  \033[36m%-10s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST) | sort | uniq)"
 
 .PHONY: root_check
 root_check: # Check if target was run as root
